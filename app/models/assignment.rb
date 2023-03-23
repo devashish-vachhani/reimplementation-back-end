570d11e093a24ec338b8079cea4dc001d409de0a
kkunnum <kkunnum@ncsu.edu>
2023-03-23 17:52:52 -0400

    Swagger UI

--- app/models/assignment.rb
+++
@@ -1,5 +0,0 @@
-class Assignment < ApplicationRecord
-    has_many :participants, class_name: 'AssignmentParticipant', foreign_key: 'parent_id', dependent: :destroy
-    def self.participants
-    end
-end
--- spec/requests/api/v1/participants_spec.rb
+++ spec/requests/api/v1/participants_spec.rb
@@ -1,33 +1,37 @@
 require 'swagger_helper'
-require 'assignment.rb'
 require 'spec_helper'
 require 'rails_helper'
+
+class Assignment < ApplicationRecord
+  belongs_to :participant
+end
+
+class AssignmentParticipant < ApplicationRecord
+end
+
+class Participant < ApplicationRecord
+  has_many :assignments
+end
 
 RSpec.describe 'api/v1/participants', type: :request do
   let(:model) { 'Assignment' }
   let(:id) { 1 }
-  let(:assignment) { class_double(Assignment) }
-  let(:an_assignment) { instance_double(Assignment) }#, id: 1) }
-  let(:participant) {class_double(Participant)}
-  let(:participants_array) {[]}
+  let(:an_assignment) { double("assignment") }
+  let(:participants_array) {[double("participant")]}
 
-  before do
-    allow(assignment).to receive(:has_many).with(:participants)
-    allow(participant).to receive(:belongs_to).with(:assignment)
-    allow(Object).to receive(:const_get).with(model).and_return(Assignment)
-    allow(an_assignment).to receive(:participants).and_return(participants_array)
-  end
 
   path '/api/v1/participants/index/{model}/{id}' do
     parameter name: 'model', in: :path, type: :string, description: 'Assignment'
     parameter name: 'id', in: :path, type: :integer, description: 1
 
     get('index participants') do
-      response(200, 'successful') do
+      response(200, 'successful') do   
         after do |example|
+          allow(Object).to receive(:k).with(1).and_return("K")
           example.metadata[:response][:content] = {
             'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
+              example: JSON.parse(response.body, model_object: "Assignment", participants:[])
+              #example: JSON.parse(response.body, error: "Missing or invalid required parameters")
             }
           }
         end
@@ -35,6 +39,7 @@ RSpec.describe 'api/v1/participants', type: :request do
       end
     end
   end
+end
 
   # path '/api/v1/participants/{model}/{id}/{authorization}' do
   #   # You'll want to customize the parameter types...
@@ -63,11 +68,12 @@ RSpec.describe 'api/v1/participants', type: :request do
   # path '/api/v1/participants/change_handle/{id}' do
   #   # You'll want to customize the parameter types...
   #   parameter name: 'id', in: :path, type: :string, description: 'id'
-
   #   post('update_handle participant') do
   #     response(200, 'successful') do
   #       let(:id) { '123' }
-
+  #       let(:participant_params) { { handle: 'new_handle' } }
+  #       let(:participant) { instance_double(AssignmentParticipant) }
+  
   #       after do |example|
   #         example.metadata[:response][:content] = {
   #           'application/json' => {
@@ -75,10 +81,19 @@ RSpec.describe 'api/v1/participants', type: :request do
   #           }
   #         }
   #       end
-  #       run_test!
+  
+  #       it 'updates the participant handle' do
+  #         expect(AssignmentParticipant).to receive(:find).with(id).and_return(participant)
+  #         expect(participant).to receive(:update).with(participant_params).and_return(true)
+  #         expect(participant).to receive(:handle).and_return('old_handle')
+  
+  #         run_test!
+  #       end
   #     end
   #   end
   # end
+  
+  
 
   # path '/api/v1/participants/update_authorizations/{id}/{authorization}' do
   #   # You'll want to customize the parameter types...
@@ -161,4 +176,4 @@ RSpec.describe 'api/v1/participants', type: :request do
   #     end
   #   end
   # end
-end
+#end
